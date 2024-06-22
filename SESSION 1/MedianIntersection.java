ae099db77be4232455c4c65f9992c166a382a907
Charan <132536897+CharanGundlapalli@users.noreply.github.com>
2024-06-22 13:44:15 +0530

    Update MedianIntersection.java

--- SESSION 1/MedianIntersection.java
+++ SESSION 1/MedianIntersection.java
@@ -2,9 +2,7 @@ public class MedianIntersection {
     public static void main(String[] args) {
         int[] array1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int[] array2 = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-        
         int medianIndex = findMedianIntersection(array1, array2);
-        
         if (medianIndex != -1) {
             System.out.println("Median intersection index: " + medianIndex);
             System.out.println("Median intersection value: " + array1[medianIndex]);
@@ -12,15 +10,14 @@ public class MedianIntersection {
             System.out.println("No intersection found.");
         }
     }
-    
     public static int findMedianIntersection(int[] arr1, int[] arr2) {
         for (int i = 0; i < arr1.length; i++) {
             for (int j = 0; j < arr2.length; j++) {
                 if (arr1[i] == arr2[j]) {
-                    return i; // Return index of first intersection found
+                    return i;
                 }
             }
         }
-        return -1; // No intersection found
+        return -1;
     }
 }
