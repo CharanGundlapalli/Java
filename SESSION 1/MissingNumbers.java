ed6c35bc51ae5539e3d1a78c284ac0ec77c2e7b9
Charan <132536897+CharanGundlapalli@users.noreply.github.com>
2024-06-22 13:44:39 +0530

    Update MissingNumbers.java

--- SESSION 1/MissingNumbers.java
+++ SESSION 1/MissingNumbers.java
@@ -1,10 +1,9 @@
 public class MissingNumbers {
     public static void main(String[] args) {
         int[] series = {1, 5, 11, 19};
-        
         System.out.println("Missing numbers:");
         for (int i = 1; i < series.length; i++) {
-            int expected = series[i-1] + 4; // Difference between consecutive numbers
+            int expected = series[i-1] + 4;
             while (expected < series[i]) {
                 System.out.print(expected + " ");
                 expected += 4;
