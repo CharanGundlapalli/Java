ac6b94ce329349ce8ece580621a7e0c0929f5d69
Charan <132536897+CharanGundlapalli@users.noreply.github.com>
2024-06-22 13:45:12 +0530

    Update PrimeNumbersInArray.java

--- SESSION 1/PrimeNumbersInArray.java
+++ SESSION 1/PrimeNumbersInArray.java
@@ -1,8 +1,7 @@
 public class PrimeNumbersInArray {
     public static void main(String[] args) {
         int[] numbers = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-        int primeCount = 0;
-        
+        int primeCount = 0;   
         System.out.println("Prime numbers in the array:");
         for (int number : numbers) {
             if (isPrime(number)) {
@@ -13,8 +12,6 @@ public class PrimeNumbersInArray {
         
         System.out.println("\nTotal prime numbers: " + primeCount);
     }
-    
-    // Function to check if a number is prime
     public static boolean isPrime(int number) {
         if (number <= 1) {
             return false;
